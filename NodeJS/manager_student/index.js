24626b3ff419ee38fb0e83134c6f9c3d1a382467
bangnx1 <bangnx1@fsoft.com.vn>
2017-10-07 22:13:25 +0700

    update delete and view function

--- NodeJS/manager_student/index.js
+++ NodeJS/manager_student/index.js
@@ -1,20 +1,41 @@
 var express = require('express');
 var conn = require('./public/mysql.js');
-
+var bodyParser = require('body-parser');
 var app = express();
 var port = 9999;
+
+function deletePath() {
+    return '/delete/:id';
+}
 
 app.use(express.static('public'));
 app.set('view engine','ejs');
 app.set('views','./views');
+var urlEncoded = bodyParser.urlencoded({extended : false});
 
+/**
+ * Create listen and open server
+ */
 app.listen(port,function(){
     console.log('Create server by port ' + port);
 });
 
+/**
+ * Method Get 
+ */
 app.get('/',function(req,res){
     conn.query('SELECT * FROM student_manager',function(err,result){
         if(err) throw err;
         res.render('index',{studen:result});
     });
 });
+
+/**
+ * Method update
+ */
+app.get(deletePath(),function(req,res){
+    var id = req.params.id;
+    conn.query('DELETE FROM student_manager WHERE student_id =' + id);
+    res.redirect('/');
+});
+
