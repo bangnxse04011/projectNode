c0a52af716ad57f3f571fdecd2f3131d338c897b
bangnx1 <bangnx1@fsoft.com.vn>
2018-01-06 22:03:34 +0700

    adadadada

--- NodeJS/CRUD/index.js
+++ NodeJS/CRUD/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var jwt = require('jsonwebtoken');
 var users = require('./router/users.js');
 var account = require('./router/account.js');
 var app = express();
@@ -9,6 +10,32 @@ app.set('views','./views');
 
 app.use('/users',users);
 app.use('/account',account);
+
+app.get("/",ensureToken,function(req,res){
+    res.json({
+        message : "Please login to system"
+    });
+});
+
+app.post("/",function(req,res){
+    const user = {id : 3};
+    const token = jwt.sign({user},"my_scret_key");
+    res.json({
+        token : token
+    });
+});
+
+function ensureToken(req, res, next) {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+      const bearer = bearerHeader.split(" ");
+      const bearerToken = bearer[0];
+      req.token = bearerToken;
+      next();
+    } else {
+      res.sendStatus(403);
+    }
+}
 
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
